55126bff1053c8fe1caa9d6866f2810cfd9578d6
cedric <cedric@Kantar>
2013-07-28 21:08:24 +0200

    Fonctionnalités PaaS

--- Frontend/controller/PaaSController.php
+++ Frontend/controller/PaaSController.php
@@ -5,6 +5,15 @@
  * and open the template in the editor.
  */
 
+include_once "../class/RestClient.php";
 
+if (isset($_POST["send"])) {
+    //$POST['template'] = "test";
+    $response = post("iaas/configure.json", $_POST);
 
+    //$response = get("rbvmomi/configuration.json");
+
+    header('location: ../view/home.php');
+    return $response;
+}
 ?>
